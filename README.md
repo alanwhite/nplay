7818950960a5aac46030ad369e47ffc616a5ae9b
Alan White <alan@whitemail.net>
2017-03-08 14:15:53 +0100

    add private APi, obscure creds

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-Play zone for node.js, aws lambda, serverless, travis CD
+Play zone for node.js, aws lambda, serverless, travis CD, Auth0
 
+Starting Point.
+Developing workflows and baseline for the back end of a modern multi-cloud app, designed to be resilient to complete cloud failures, e.g. AWS goes dark, or Azure / GCP. Doesn't try to leverage internal availability mechanisms such as AZs, uses two public cloud infratructure providers and the 3rd party Auth0 service so as not to be dependent on proprietary id and auth mechanisms in each provider.
+
+Let's see where we end up!
--- nplay.js
+++ nplay.js
@@ -1,16 +1,62 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET;
+
+// Policy helper function
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+
+// Reusable Authorizer function, set on `authorizer` field in serverless.yml
+module.exports.auth = (event, context, cb) => {
+  if (event.authorizationToken) {
+    // remove "bearer " from token
+    const token = event.authorizationToken.substring(7);
+    const options = {
+      audience: AUTH0_CLIENT_ID,
+    };
+    jwt.verify(token, AUTH0_CLIENT_SECRET, options, (err, decoded) => {
+      if (err) {
+        cb('Unauthorized');
+      } else {
+        cb(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+      }
+    });
+  } else {
+    cb('Unauthorized');
+  }
+};
+
+// public API
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! Public API says Hi!',
       input: event,
     }),
   };
 
   callback(null, response);
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+// Private API
+module.exports.privateEndpoint = (event, context, cb) => {
+  cb(null, { message: 'Only logged in users can see this' });
 };
