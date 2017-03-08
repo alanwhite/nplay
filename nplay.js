7c054d8d5be357406c7f3a1498af0bda1975428f
Alan White <alan@whitemail.net>
2017-03-08 23:23:20 +0100

    cors

--- nplay.js
+++ nplay.js
@@ -47,6 +47,9 @@ module.exports.auth = (event, context, cb) => {
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers: {
+       "Access-Control-Allow-Origin" : "*"
+    },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Public API says Hi!',
       input: event,
